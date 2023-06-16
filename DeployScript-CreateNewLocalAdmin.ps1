e2419a33b49f3610892d4b97dbbac6db1f1d9db4
dylanreynolds <dylan.w.reynolds@gmail.com>
2023-06-16 14:41:08 +1000

    Update DeployScript-CreateNewLocalAdmin.ps1

--- DeployScript-CreateNewLocalAdmin.ps1
+++ DeployScript-CreateNewLocalAdmin.ps1
@@ -1,17 +1,15 @@
 # Edit the URL to your blob storage path
-$PSurl= ""
+$PSurl= "https://raw.githubusercontent.com/dylanreynolds/CreateLocalUser/main/CreateLocalUserviaIntunewithLogging.ps1"
 
 # Location where we will add the script to run on logon
-$regKeyLocation="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$regKeyLocation="HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 
 # Command for the registry
 $psCommand= "PowerShell.exe -ExecutionPolicy Bypass -Windowstyle hidden -command $([char]34)& {(Invoke-RestMethod '$PSurl').Replace('ï','').Replace('»','').Replace('¿','') | Invoke-Expression}$([char]34)"
 
 # Check if the registry location exist, if not create it.
 if (-not(Test-Path -Path $regKeyLocation)) {
-
     New-ItemProperty -Path $regKeyLocation -Force
-
 }
 
 # Create / Update the registry to reflect the powershell command.
